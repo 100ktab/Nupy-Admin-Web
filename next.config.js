bc5de3d08bf669299e2c8937a001325d527c34f5
joy <chohee1231@gmail.com>
2023-07-16 02:10:10 +0900

    change key name

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
     STABILITY_API_KEY: process.env.STABILITY_API_KEY,
     AWS_ACCESS_ID: process.env.AWS_ACCESS_ID,
-    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_ACCESS_KEY: process.env.AWS_PERSONAL_SECRET_KEY,
     AWS_REGION_KEY: process.env.AWS_REGION_KEY,
     WEB3_STORAGE_API_KEY: process.env.WEB3_STORAGE_API_KEY
   }
